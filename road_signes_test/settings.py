6c4d058541ccbdbedbcc9a2ebd5ad26e8ae76d43
Typeef <typeef84@gmail.com>
2026-03-27 21:57:25 +0300

    fix: static

--- road_signes_test/settings.py
+++ road_signes_test/settings.py
@@ -78,7 +78,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # <-- Добавьте эту строку
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
